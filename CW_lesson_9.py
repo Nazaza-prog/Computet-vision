176e5abf2232d967520a1160ec22f3614d2c8d15
Nazar <142441304+Nazaza-prog@users.noreply.github.com>
2026-03-13 10:00:02 +0200

    Add files via upload

---
+++ CW_lesson_9.py
@@ -0,0 +1,41 @@
+import cv2
+
+net = cv2.dnn.readNetFromCaffe('data/MobileNet/mobilenet_v2_deploy.prototxt', 'data/MobileNet/mobilenet_v2.caffemodel')
+classes = []
+
+# фільтрація
+with open('data/MobileNet/synset.txt', 'r', encoding='utf-8') as f:
+    for line in f:
+        line = line.strip()
+        if not line:
+            continue
+
+        parts = line.split(" ", 1)
+        name = parts[1] if len(parts) > 1 else parts[0]
+        classes.append(name)
+
+
+image = cv2.imread('images/people.jpg')
+blob = cv2.dnn.blobFromImage(cv2.resize(image, (224, 224)), 1.0 / 127.5, (224, 224), (127.5, 127.5, 127.5))
+
+
+net.setInput(blob)
+# ймовірність
+preds = net.forward()
+
+index = preds[0].argmax()
+
+label = classes[index] if index < len(classes) else "unknown"
+conf = float(preds[0][index].item()) * 100
+
+print(f'Клас: {label}')
+print(f'Ймовірнітсь: {round(conf, 2)}%')
+
+
+text = label + ": " + str(int(conf)) + "%"
+cv2.putText(image, text, (10, 30), cv2.FONT_HERSHEY_TRIPLEX, 0.7, (0, 255, 0), 2)
+
+
+cv2.imshow("result", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows
